095272af5b744f857b9e08fd6f893e95b1a452fb
dp2genius <dp2genius@outlook.com>
2023-11-01 08:08:34 -0400

    chore: change about-body

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,37 +19,16 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              Highly skilled and innovative Full Stack AI Developer with a strong proficiency in web development and
+              AI technologies. Passionate about creating cutting-edge AI applications and integrating machine
+              learning models with web applications.
+              Able to guide the architecture and user experience design for full-fledged AI applications while
+              providing expert-level insights and solutions to complex AI problems.
+              Experienced in utilizing TensorFlow, Keras, PyTorch, and various other AI libraries to implement
+              computer vision and natural language processing models. Proficient in developing neural networks and
+              deploying them in cloud environments.
+              Skilled in data visualization using tools such as Matplotlib and D3.js, etc. to present insights from
+              machine learning models.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
