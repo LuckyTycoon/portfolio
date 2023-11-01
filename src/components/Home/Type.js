b9f8f6945bc96882a579bce6497f543160fa37e3
dp2genius <dp2genius@gmail.com>
2023-11-01 06:57:09 -0400

    chore: change type writer settings

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Full stack web developer",
+          "AI engineer",
         ],
         autoStart: true,
         loop: true,
